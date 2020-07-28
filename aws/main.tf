fc38e337425119a04cdaa0ac9ae965d63eef43f8
kenshi08 <kenshi08@me.com>
2020-07-28 15:39:07 +0800

    modified vars

--- aws/main.tf
+++ aws/main.tf
@@ -1,157 +1,146 @@
-#Configure the AWS provider
+terraform {
+  required_version = ">= 0.12"
+}
+
 provider "aws" {
-  version = "~> 2.18"
-  region = "${var.vpc_region}"
-  access_key = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
+  region = var.aws_region
 }
 
-# Define a vpc
-resource "aws_vpc" "vpc_name" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "${var.vpc_name}"
-  }
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
 }
 
-# Internet gateway for the public subnet
-resource "aws_internet_gateway" "dev_ig" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  tags = {
-    Name = "operationsaasgateway"
-  }
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
 }
 
-# Public subnet
-resource "aws_subnet" "vpc_public_sn" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  cidr_block = "${var.vpc_public_subnet_cidr}"
-  #availability_zone = "${lookup(var.availability_zone, var.vpc_region)}"
-  tags = {
-    Name = "OperationsaasPublicSubnet"
-  }
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
 }
 
-# Private subnet
-resource "aws_subnet" "vpc_private_sn" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  cidr_block = "${var.vpc_private_subnet_cidr}"
-  #availability_zone = "${vpc_region}
-  tags = {
-    Name = "OperationsaasPrivateSubnet"
-  }
+# Create a subnet to launch our instances into
+resource "aws_subnet" "default" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
-# Routing table for public subnet
-resource "aws_route_table" "vpc_public_sn_rt" {
-  vpc_id = "${aws_vpc.vpc_name.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.dev_ig.id}"
-  }
-  tags = {
-    Name = "operationsaasrt"
-  }
-}
+# A security group for the ELB so it is accessible via the web
+resource "aws_security_group" "elb" {
+  name        = "terraform_example_elb"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.default.id
 
-# Associate the routing table to public subnet
-resource "aws_route_table_association" "vpc_public_sn_rt_assn" {
-  subnet_id = "${aws_subnet.vpc_public_sn.id}"
-  route_table_id = "${aws_route_table.vpc_public_sn_rt.id}"
-}
-
-# Security group
-resource "aws_security_group" "vpc_public_sg" {
-  name = "dev_pubic_sg"
-  description = "dev public access security group"
-  vpc_id = "${aws_vpc.vpc_name.id}"
-
+  # HTTP access from anywhere
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = [
-      "${var.vpc_access_from_ip_range}"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "tcp"
-    cidr_blocks = [
-      "${var.vpc_public_subnet_cidr}"]
-  }
-
+  # outbound internet access
   egress {
-    # allow all traffic to private SN
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-  tags = {
-    Name = "operationsaaseg"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_security_group" "vpc_private_sg" {
-  name = "dev_private_sg"
-  description = "dev security group to access private ports"
-  vpc_id = "${aws_vpc.vpc_name.id}"
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.default.id
 
-  # allow memcached port within VPC
+  # SSH access from anywhere
   ingress {
-    from_port = 11211
-    to_port = 11211
-    protocol = "tcp"
-    cidr_blocks = [
-      "${var.vpc_public_subnet_cidr}"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
-
-  # allow mysql port within VPC
+  # HTTP access from the VPC
   ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = [
-      "${var.vpc_public_subnet_cidr}"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
+  # outbound internet access
   egress {
-    from_port = "0"
-    to_port = "0"
-    protocol = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-  tags = {
-    Name = "operationsaaseg"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "vpc_region" {
-  value = var.vpc_region
+resource "aws_elb" "web" {
+  name = "terraform-example-elb"
+
+  subnets         = [aws_subnet.default.id]
+  security_groups = [aws_security_group.elb.id]
+  instances       = [aws_instance.web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
 }
 
-output "vpc_id" {
-  value = "${aws_vpc.vpc_name.id}"
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
 }
 
-output "vpc_public_sn_id" {
-  value = "${aws_subnet.vpc_public_sn.id}"
-}
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    type = "ssh"
+    # The default username for our AMI
+    user = "ubuntu"
+    host = self.public_ip
+    # The connection will use the local SSH agent for authentication.
+  }
 
-output "vpc_private_sn_id" {
-  value = "${aws_subnet.vpc_private_sn.id}"
-}
+  instance_type = "t2.micro"
 
-output "vpc_public_sg_id" {
-  value = "${aws_security_group.vpc_public_sg.id}"
-}
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = var.aws_amis[var.aws_region]
 
-output "vpc_private_sg_id" {
-  value = "${aws_security_group.vpc_private_sg.id}"
+  # The name of our SSH keypair we created above.
+  key_name = aws_key_pair.auth.id
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.default.id
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
+  }
 }
