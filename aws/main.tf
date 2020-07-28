c4365891ea7fa90670116ce03a2476105032bc7c
kenshi08 <kenshi08@me.com>
2020-07-28 16:31:39 +0800

    removed ssh agent

--- aws/main.tf
+++ aws/main.tf
@@ -101,16 +101,6 @@ resource "aws_elb" "web" {
 }
 
 resource "aws_instance" "web" {
-  # The connection block tells our provisioner how to
-  # communicate with the resource (instance)
-  connection {
-    type = "ssh"
-    # The default username for our AMI
-    user = "ubuntu"
-    host = self.public_ip
-    # The connection will use the local SSH agent for authentication.
-  }
-
   instance_type = "t2.micro"
 
   # Lookup the correct AMI based on the region
@@ -124,15 +114,4 @@ resource "aws_instance" "web" {
   # environment it's more common to have a separate private subnet for
   # backend instances.
   subnet_id = aws_subnet.default.id
-
-  # We run a remote provisioner on the instance after creating it.
-  # In this case, we just install nginx and start it. By default,
-  # this should be on port 80
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get -y install nginx",
-      "sudo service nginx start",
-    ]
-  }
 }
